fd6e48effa5f28c1a77fc4bd90ee3c3669aeac04
jmarkmorris <johnmarkmorris@gmail.com>
2025-04-01 19:11:24 -0400

    feat: Add script to run square scene with low quality rendering

---
+++ square/run_square.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Smallest possible square size in manim (theoretically, but limited by computational precision)
+# Largest square size that will fit: frame_width
+
+manim -pql square/square.py TiledSquares
