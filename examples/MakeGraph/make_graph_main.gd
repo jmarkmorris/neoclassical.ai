0a32c7c951e443add759f0efff56ae4a46dfea0f
jmarkmorris <johnmarkmorris@gmail.com>
2025-04-07 22:08:35 -0400

    refactor: change axis colors from gray to white for better visibility

--- examples/MakeGraph/make_graph_main.gd
+++ examples/MakeGraph/make_graph_main.gd
@@ -99,11 +99,11 @@ func create_axes() -> void:
 	var y_max = 5.0 # Extended y-axis upper bound for GRAPH_VERTICAL_STRETCH=4.4
 
 	# Create thick x-axis
-	var x_axis_mesh_instance = create_thick_line_mesh(Vector3(x_min, 0, 0), Vector3(x_max, 0, 0), 0.01, Color.GRAY)
+	var x_axis_mesh_instance = create_thick_line_mesh(Vector3(x_min, 0, 0), Vector3(x_max, 0, 0), 0.01, Color.WHITE)
 	axes.add_child(x_axis_mesh_instance)
 
 	# Create thick y-axis
-	var y_axis_mesh_instance = create_thick_line_mesh(Vector3(0, y_min, 0), Vector3(0, y_max, 0), 0.01, Color.GRAY) # Use updated limits
+	var y_axis_mesh_instance = create_thick_line_mesh(Vector3(0, y_min, 0), Vector3(0, y_max, 0), 0.01, Color.WHITE) # Use updated limits
 	axes.add_child(y_axis_mesh_instance)
 
 	# Create x-axis ticks
